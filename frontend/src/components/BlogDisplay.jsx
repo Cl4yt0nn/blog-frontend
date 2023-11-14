1889bcbb008c1cc00c737caedd95c3b6b028b08c
Cl4yt0nn <111470334+Cl4yt0nn@users.noreply.github.com>
2023-11-14 12:53:10 -0700

    Add files via upload

---
+++ frontend/src/components/BlogDisplay.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState} from 'react'
+import axios from 'axios';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+
+function HomePage() {
+    
+    const [blogForm, setBlogForm] = useState([]);
+    const [recommended, setRecommended] = useState([]);
+
+    let params = useParams();
+    let navigate = useNavigate();
+
+    function bigger(x) {
+        if (x < 3) {
+            return x;
+        } else {
+            return 3;
+        }
+    }
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:10000/posts/view/" + params.id)
+            .then((res) => {
+                setBlogForm(res.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:10000/posts/display-post")
+            .then((res) => {
+                let blogList = res.data.data;
+                console.log(blogList);
+            
+                const shuffledArray = blogList.slice(); // Create a copy of the input array
+                for (let i = shuffledArray.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Shuffle the array
+                }
+    
+                const newRecommended = shuffledArray.slice(0, bigger(blogList.length));
+    
+                setRecommended(newRecommended);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    useEffect(() => {
+        console.log(recommended);
+    }, [recommended])
+
+    return (   
+        <div className="dispContainer">
+            <div className="dispBody">
+                <h1 className="dispTitle">{blogForm.title}</h1>
+                <h2 className="dispAuthor">By: {blogForm.author}</h2>
+                <img src={blogForm.imageURL} alt="image" className="dispImg" />
+                <p className="dispText">{blogForm.text}</p>
+            </div>
+            <div className="dispRecommended">
+                {recommended.map((blog, key) => {
+                    return (
+                        <div className="dispRecBlog" key={key} onClick={() => location.replace(`/view/${blog._id}`)}>
+                            <img src={blog.imageURL} alt="img" className="recLeft" />
+                            <div className="recRight">
+                                <h2>{blog.title}</h2>
+                                <div>{blog.description}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+);
+}
+
+
+            
+
+export default HomePage;
---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState} from 'react'
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+    
+    const [blogForm, setBlogForm] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:10000/posts/display-post")
+            .then((res) => {
+                setBlogForm(res.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []); 
+
+    useEffect(() => {
+        console.log(blogForm)
+    }, [blogForm])
+
+    return (   
+        <div className="blogsDisplay">
+            {blogForm.map((blog, index) => {
+                return (
+                    <Link to={`/view/${blog._id}`} className="blog" key={index}>
+                        <img src={blog.imageURL} alt={blog.title + " image"} width="100" />
+                        <h1 className="hph1">{blog.title}</h1><br />
+                        <h2 className="hph2">{blog.author}</h2><br />
+                        <p className="hpp">{blog.description}</p>
+                    </Link>
+                );
+            })}
+        </div>
+);
+}
+
+
+            
+
+export default HomePage;
+
+
+
+       
